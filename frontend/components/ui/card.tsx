9214d232bf1386cdc038a44c6e6f76d89286073d
Evgeny Tatarintsev <evtatarintsev@ya.ru>
2025-06-01 11:57:47 +0300

    refactoring Card

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -1,79 +1,77 @@
 import * as React from "react"
+import {twMerge} from "tailwind-merge"
 
-import { cn } from "@/lib/utils"
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+}
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
-      className
-    )}
-    {...props}
-  />
-))
-Card.displayName = "Card"
+interface CardHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+}
 
-const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props}
-  />
-))
-CardHeader.displayName = "CardHeader"
+interface CardTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
+}
 
-const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
->(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className
-    )}
-    {...props}
-  />
-))
-CardTitle.displayName = "CardTitle"
+interface CardDescriptionProps extends React.HTMLAttributes<HTMLParagraphElement> {
+}
 
-const CardDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-CardDescription.displayName = "CardDescription"
+interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {
+}
 
-const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
+interface CardFooterProps extends React.HTMLAttributes<HTMLDivElement> {
+}
 
-const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props}
-  />
-))
-CardFooter.displayName = "CardFooter"
+function Card({className, ...props}: CardProps) {
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+    return (
+        <div
+            className={twMerge("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+            {...props}
+        />
+    )
+}
+
+function CardHeader({className, ...props}: CardHeaderProps) {
+    return (
+        <div
+            className={twMerge("flex flex-col space-y-1.5 p-6", className)}
+            {...props}
+        />
+    )
+}
+
+function CardTitle({className, ...props}: CardTitleProps) {
+    return (
+        <h3
+            className={twMerge(
+                "text-2xl font-semibold leading-none tracking-tight",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+
+function CardDescription({className, ...props}: CardDescriptionProps) {
+    return (
+        <p
+            className={twMerge("text-sm text-muted-foreground", className)}
+            {...props}
+        />
+    )
+}
+
+function CardContent({className, ...props}: CardContentProps) {
+    return (
+        <div className={twMerge("p-6 pt-0", className)} {...props} />
+    )
+}
+
+function CardFooter({className, ...props}: CardFooterProps) {
+    return (
+        <div
+            className={twMerge("flex items-center p-6 pt-0", className)}
+            {...props}
+        />
+    )
+}
+
+export {Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent}
