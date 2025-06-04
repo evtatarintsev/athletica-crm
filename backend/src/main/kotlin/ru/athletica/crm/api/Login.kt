065127c99e1edce3b0276282f0d347db8bc1a923
Evgeny Tatarintsev <evtatarintsev@ya.ru>
2025-06-04 18:44:27 +0300

    add login controller

---
+++ backend/src/main/kotlin/ru/athletica/crm/api/Login.kt
@@ -0,0 +1,20 @@
+package ru.athletica.crm.api
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.athletica.api.schemas.LoginRequest
+import ru.athletica.api.schemas.LoginResponse
+import ru.athletica.crm.db.suspendTransaction
+
+
+@RestController
+@RequestMapping("/api/login")
+class Login() {
+
+    @PostMapping
+    suspend fun login(request: LoginRequest): LoginResponse = suspendTransaction {
+        println(request)
+        LoginResponse(token = request.login)
+    }
+}
